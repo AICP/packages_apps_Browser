d5f750cbab2cbc2b8a07ae0d5e006ebb036ec529
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-12-14 09:17:31 +0700

    Browser: fix pie crash
    Introduced in 5623a091605bf1d88687dc589a61ee6b54c554fe.
    
    Change-Id: I83f44be84fb70e2777ab1414c0af2a368c435fb8

--- src/com/android/browser/PieControl.java
+++ src/com/android/browser/PieControl.java
@@ -182,6 +182,7 @@ public class PieControl implements PieMenu.PieController, OnClickListener {
         mUrl.addItem(makeFiller());
         mUrl.addItem(makeFiller());
         mPie.addItem(mShowTabs);
+        mShowTabs.addItem(makeFiller());
         mShowTabs.addItem(mClose);
         mShowTabs.addItem(mIncognito);
         mShowTabs.addItem(mNewTab);
